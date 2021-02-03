fe67071112222c92682930969e5590c4d772e2bd
bolladaniele <bolladaniele@gmail.com>
2021-02-03 12:36:58 +0000

    add development mode

---
+++ examples/simple.js
@@ -0,0 +1,43 @@
+import { initVideoAdsMonetizer } from "./esm/index.js";
+
+// const vanillaCredentials = {
+//   enabled: false,
+//   clientId: "524d23c6-a0f1-422e-8d20-24dc84d419e6",
+//   clientSecret: "oPMA9k+i1jHi45ITXD6Biag+4xF9b7tcHnwnepe3MAA=",
+// };
+const receiptVerify = {
+  enabled: false,
+  apiUrl: "https://web-monetization-server-test.herokuapp.com",
+  verifyEndPoint: "verifyReceipt",
+};
+
+const paymentPointer = "$ilp.uphold.com/RzZiPnxpFYf9";
+const tagUrl = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=";
+const videoElement = document.querySelector("video");
+
+initVideoAdsMonetizer({
+  videoElement,
+  monetizationConfig: {
+    paymentPointer,
+    receiptVerify,
+  },
+  adsConfig: {
+    tagUrl,
+    live: true,
+  },
+});
+
+if (document.monetization) {
+  document.monetization.addEventListener("monetizationstart", () => {
+    console.log("monetizationstart");
+  });
+  document.monetization.addEventListener("monetizationpending", () => {
+    console.log("monetizationpending");
+  });
+  document.monetization.addEventListener("monetizationstop", () => {
+    console.log("monetizationstop");
+  });
+  document.monetization.addEventListener("monetizationprogress", () => {
+    console.log("monetizationprogress");
+  });
+}
---
+++ rollup.config.dev.js
@@ -0,0 +1,60 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import babel from "@rollup/plugin-babel";
+import pkg from "./package.json";
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+const input = ["src/index.js"];
+export default [
+  {
+    // UMD
+    input,
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: "bundled",
+        exclude: 'node_modules/**',
+      }),
+      serve({
+        open: true,
+        openPage: '/',
+        host: 'localhost',
+        port: 3003,
+        contentBase: ['./examples', './dist'],
+      }),
+      livereload({
+        watch: ['./examples'],
+        exts: ['html', 'js', 'css'],
+      }),
+      terser(),
+    ],
+    output: {
+      file: `examples/${pkg.name}.min.js`,
+      format: "umd",
+      name: "web-monetisation-video-ads", // this is the name of the global object
+      esModule: false,
+      exports: "named",
+      sourcemap: true,
+    },
+  },
+  // ESM and CJS
+  {
+    input,
+    plugins: [nodeResolve()],
+    output: [
+      {
+        dir: "dist/esm",
+        format: "esm",
+        exports: "named",
+        sourcemap: true,
+      },
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+  },
+];
