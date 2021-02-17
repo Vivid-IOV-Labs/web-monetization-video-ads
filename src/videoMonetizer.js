b9b53c23a0316860fc6ef82ebac61d092bbf6e6b
bolladaniele <bolladaniele@gmail.com>
2021-02-17 10:50:31 +0000

    comment isActiveTab

--- src/videoMonetizer.js
+++ src/videoMonetizer.js
@@ -107,30 +107,30 @@ const monetizationChecker = ({
   });
 };
 
-const isActiveTab = function (handleVisibilityChange) {
-  var hidden, visibilityChange;
-  if (typeof document.hidden !== "undefined") {
-    hidden = "hidden";
-    visibilityChange = "visibilitychange";
-  } else if (typeof document.msHidden !== "undefined") {
-    hidden = "msHidden";
-    visibilityChange = "msvisibilitychange";
-  } else if (typeof document.webkitHidden !== "undefined") {
-    hidden = "webkitHidden";
-    visibilityChange = "webkitvisibilitychange";
-  }
-  function handler() {
-    handleVisibilityChange(document[hidden]);
-  }
-  if (
-    typeof document.addEventListener === "undefined" ||
-    hidden === undefined
-  ) {
-    throw new Error("Page Visibility API not enabled");
-  } else {
-    document.addEventListener(visibilityChange, handler, false);
-  }
-};
+// const isActiveTab = function (handleVisibilityChange) {
+//   var hidden, visibilityChange;
+//   if (typeof document.hidden !== "undefined") {
+//     hidden = "hidden";
+//     visibilityChange = "visibilitychange";
+//   } else if (typeof document.msHidden !== "undefined") {
+//     hidden = "msHidden";
+//     visibilityChange = "msvisibilitychange";
+//   } else if (typeof document.webkitHidden !== "undefined") {
+//     hidden = "webkitHidden";
+//     visibilityChange = "webkitvisibilitychange";
+//   }
+//   function handler() {
+//     handleVisibilityChange(document[hidden]);
+//   }
+//   if (
+//     typeof document.addEventListener === "undefined" ||
+//     hidden === undefined
+//   ) {
+//     throw new Error("Page Visibility API not enabled");
+//   } else {
+//     document.addEventListener(visibilityChange, handler, false);
+//   }
+// };
 
 const playPauseVideoHandler = ({ videoElement, paymentPointer }) => {
   videoElement.addEventListener("play", () => {
@@ -200,13 +200,13 @@ export const initVideoMonetizer = ({
   } else {
     dispatchEvent("monetization-enabled");
 
-    isActiveTab((isActive) => {
-      if (isActive) {
-        startMonetization();
-      } else {
-        stopMonetization();
-      }
-    });
+    // isActiveTab((isActive) => {
+    //   if (isActive) {
+    //     startMonetization();
+    //   } else {
+    //     stopMonetization();
+    //   }
+    // });
 
     monetizationChecker({ videoElement, vanillaCredentials, receiptVerify });
 
