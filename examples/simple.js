6ead424dcf07943c626a5737335de7c3a7b080bb
bolladaniele <bolladaniele@gmail.com>
2021-04-15 10:17:35 +0100

    removes readme

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,9 @@
 import { initVideoAdsMonetizer } from "./esm/index.js";
 
-const paymentPointer = "$custom-paymentpointer";
+const paymentPointer = "$ilp.uphold.com/m4nWWXDUwibP";
+//btc const paymentPointer = "$ilp.uphold.com/RzZiPnxpFYf9";
+//xrp $ilp.uphold.com/YrYiPeFRFKKJ
+// ether $ilp.uphold.com/m4nWWXDUwibP
 
 const tagUrl =
   "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=";
@@ -18,7 +21,7 @@ document.addEventListener(
         stopOnInactiveTab: false,
         paymentPointer,
         fakeMonetization: {
-          enabled: true,
+          enabled: false,
         },
       },
       adsConfig: {
