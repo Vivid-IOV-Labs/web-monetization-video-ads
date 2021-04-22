cdfff8a4215deb9c191b2a2a786818ede3f85b71
bolladaniele <bolladaniele@gmail.com>
2021-04-22 20:25:23 +0100

    update licence

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,11 @@
 import { initVideoAdsMonetizer } from "./esm/index.js";
 
-const paymentPointer = "$custompaymentpointer";
+//const paymentPointer = "$custompaymentpointer";
+const paymentPointer = "$ilp.uphold.com/hz28FmDDH3mk";
+//btc $ilp.uphold.com/RzZiPnxpFYf9;
+//xrp $ilp.uphold.com/YrYiPeFRFKKJ
+//ether $ilp.uphold.com/m4nWWXDUwibP
+//doge $ilp.uphold.com/hz28FmDDH3mk
 
 const tagUrl =
   "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=";
